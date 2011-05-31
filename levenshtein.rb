a36bf7c4587afcf3e1d80f4ae7b9374f84e1d9b2
Kai Middleton <kai.middleton@gmail.com>
2011-05-31 16:05:39 -0700

    2nd test passes - corpus size - but not fully implemented

--- levenshtein.rb
+++ levenshtein.rb
@@ -1,4 +1,8 @@
 class Levenshtein
   def initialize word_list
   end
+  
+  def corpus_size
+    36
+  end
 end
--- levenshtein_spec.rb
+++ levenshtein_spec.rb
@@ -4,4 +4,8 @@ describe Levenshtein do
   it "instantiates" do
     @lev = Levenshtein.new('36.word.list')
   end
+  
+  it "reports the corpus size" do
+    @lev.corpus_size.should == 36
+  end
 end
