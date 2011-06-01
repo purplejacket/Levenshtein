b338a0df5c25789ff5079b30471d1c096c7dd237
Kai Middleton <kai.middleton@gmail.com>
2011-06-01 14:05:46 -0700

    test the listing of variants with a larger perimeter

--- perimeter_hash_spec.rb
+++ perimeter_hash_spec.rb
@@ -6,16 +6,29 @@ describe PerimeterHash do
     @corpus = {'aa' => true, 'a' => true, 'ab' => true, 'xaa' => true, 'mississippi' => true}
   end
   
-  it "lists variants" do
-    perimeter = {'aa' => true}
-    expected_variants =  %w(ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az)
-    expected_variants += %w(ba ca da ea fa ga ha ia ja ka la ma na oa pa qa ra sa ta ua va wa xa ya za)
-    expected_variants += %w(aaa baa caa daa eaa faa gaa haa iaa jaa kaa laa maa naa oaa paa qaa raa saa taa uaa vaa waa xaa yaa zaa)
-    expected_variants += %w(aba aca ada aea afa aga aha aia aja aka ala ama ana aoa apa aqa ara asa ata aua ava awa axa aya aza)
-    expected_variants += %w(aab aac aad aae aaf aag aah aai aaj aak aal aam aan aao aap aaq aar aas aat aau aav aaw aax aay aaz)
-    expected_variants += %w(a)
-    @phash = PerimeterHash.new(perimeter, @corpus)
-    variants = @phash.variants
-    Set.new(variants).should == Set.new(expected_variants)
+  describe "variants" do
+    before do
+      perimeter1 = {'aa' => true}
+      expected_variants1 =  %w(ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az)
+      expected_variants1 += %w(ba ca da ea fa ga ha ia ja ka la ma na oa pa qa ra sa ta ua va wa xa ya za)
+      expected_variants1 += %w(aaa baa caa daa eaa faa gaa haa iaa jaa kaa laa maa naa oaa paa qaa raa saa taa uaa vaa waa xaa yaa zaa)
+      expected_variants1 += %w(aba aca ada aea afa aga aha aia aja aka ala ama ana aoa apa aqa ara asa ata aua ava awa axa aya aza)
+      expected_variants1 += %w(aab aac aad aae aaf aag aah aai aaj aak aal aam aan aao aap aaq aar aas aat aau aav aaw aax aay aaz)
+      expected_variants1 += %w(a)
+      
+      perimeter2 = {'a' => true, 'aa' => true}
+      expected_variants2 = Array.new expected_variants1
+      expected_variants2 += %w(b c d e f g h i j k l m n o p q r s t u v w x y z)
+      
+      @test_values = {perimeter1 => expected_variants1, perimeter2 => expected_variants2}
+    end
+    
+    @test_values.each_pair do |perimeter, expected_variants|
+      it "list correctly" do
+        phash = PerimeterHash.new(perimeter, @corpus)
+        variants = phash.variants
+        Set.new(variants).should == Set.new(expected_variants)
+      end
+    end
   end
 end
