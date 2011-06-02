0f31963e36dbadfca48378887e2fd5b956bb6b16
Kai Middleton <kai.middleton@gmail.com>
2011-06-02 15:05:36 -0700

    big refactoring, changed tests too: changed perimeter from array to hash.  Also, perimeter.variants no longer has any intersection with perimeter

--- perimeter_words.rb
+++ perimeter_words.rb
@@ -1,14 +1,17 @@
+require 'ruby-debug'
+
 class PerimeterWords
-  def initialize perimeter, corpus
+  def initialize perimeter, corpus, words_inside_clique
     @perimeter = perimeter
     @corpus = corpus
+    @words_inside_clique = words_inside_clique
   end
   
   def variants
-    @perimeter.inject([]) { |accumulator, word| accumulator += levenshtein_word_variants(word) }
+    @perimeter.keys.inject({}) { |accumulator, word| accumulator.merge! levenshtein_word_variants(word) }
   end
   
-  def hits
+  def next_perimeter
     variants.inject([]) { |accumulator, variant| @corpus[variant] ? (accumulator << variant) : accumulator }
   end
   
@@ -19,47 +22,46 @@ class PerimeterWords
   private
   
   def levenshtein_word_variants word
-    retval = []
-    retval += add_a_letter_at_each_position(word)
-    retval += change_a_letter_at_each_position(word)
-    retval += remove_a_letter_at_each_position(word)
-    retval -= [word]
+    retval = {}
+    retval.merge! add_a_letter_at_each_position(word)
+    retval.merge! change_a_letter_at_each_position(word)
+    retval.merge! remove_a_letter_at_each_position(word)
     retval
   end
   
   @@alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
   
   def add_a_letter_at_each_position word
-    retval = []
+    retval = {}
     (0..(word.size)).each do |position|
       @@alphabet.each do |letter|
         w = String.new word
         w.insert(position, letter)
-        retval << w
+        retval[w] = true if not @perimeter[w]
       end
     end
     retval
   end
   
   def change_a_letter_at_each_position word
-    retval = []
+    retval = {}
     (0..(word.size - 1)).each do |position|
       @@alphabet.each do |letter|
         w = String.new word
         w[position] = letter
-        retval << w
+        retval[w] = true if not @perimeter[w]
       end
     end
     retval
   end
 
   def remove_a_letter_at_each_position word
-    return [] if word.length < 2
-    retval = []
+    return {} if word.length < 2
+    retval = {}
     (0..(word.size - 1)).each do |position|
       w = String.new word
       w[position] = ""
-      retval << w
+      retval[w] = true if not @perimeter[w]
     end
     retval
   end
--- perimeter_words_spec.rb
+++ perimeter_words_spec.rb
@@ -1,13 +1,19 @@
 require 'perimeter_words.rb'
 require 'set'
 
+class Array
+  def to_hash
+    Hash[*self.collect { |v| [v, true]}.flatten]
+  end
+end
+
 describe PerimeterWords do
   before do
-    @corpus = {'aa' => true, 'a' => true, 'ab' => true, 'xaa' => true, 'mississippi' => true}
+    @corpus = {'aa' => true, 'a' => true, 'ab' => true, 'xaa' => true, 'b' => true, 'bb' => true, 'mississippi' => true}
   end
   
   describe "variants" do
-    perimeter1 = %w(aa)
+    perimeter_hash1 = {'aa' => true}
     expected_variants1 =  %w(ab ac ad ae af ag ah ai aj ak al am an ao ap aq ar as at au av aw ax ay az)
     expected_variants1 += %w(ba ca da ea fa ga ha ia ja ka la ma na oa pa qa ra sa ta ua va wa xa ya za)
     expected_variants1 += %w(aaa baa caa daa eaa faa gaa haa iaa jaa kaa laa maa naa oaa paa qaa raa saa taa uaa vaa waa xaa yaa zaa)
@@ -15,27 +21,28 @@ describe PerimeterWords do
     expected_variants1 += %w(aab aac aad aae aaf aag aah aai aaj aak aal aam aan aao aap aaq aar aas aat aau aav aaw aax aay aaz)
     expected_variants1 += %w(a)
     
-    perimeter2 = %w(a aa)
+    perimeter_hash2 = {'aa' => true, 'a' => true}
     expected_variants2 = Array.new expected_variants1
-    expected_variants2 += %w(aa)
+    expected_variants2 -= %w(a)
     expected_variants2 += %w(b c d e f g h i j k l m n o p q r s t u v w x y z)
     
-    @test_values = {perimeter1 => expected_variants1, perimeter2 => expected_variants2}
+    @test_values = {perimeter_hash1 => expected_variants1.to_hash, perimeter_hash2 => expected_variants2.to_hash}
     
-    @test_values.each_pair do |perimeter, expected_variants|
+    @test_values.each_pair do |perimeter_hash, expected_variants|
       it "are listed correctly" do
-        pwords = PerimeterWords.new(perimeter, @corpus)
-        variants = pwords.variants
-        Set.new(variants).should == Set.new(expected_variants)
+        perimeter_words = PerimeterWords.new(perimeter_hash, @corpus, {})
+        variants = perimeter_words.variants
+        perimeter_words.variants.should == expected_variants
       end
     end
   end
   
-  describe "hits" do
-    it "are evaluated correctly" do
-      perimeter = %w(aa)
-      pwords = PerimeterWords.new(perimeter, @corpus)
-      expected_hits = %w(a ab xaa)
+  
+  describe "next_perimeter" do
+    xit "is evaluated correctly when there are no words inside the clique yet" do
+      perimeter = {'aa' => true}
+      pwords = PerimeterWords.new(perimeter, @corpus, {})
+      expected_hits = {  :todo => :TODO  }
       hits = pwords.hits
       Set.new(hits).should == Set.new(expected_hits)
     end
